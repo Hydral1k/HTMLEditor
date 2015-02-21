f57eeac2076827d70548d3f63fc035ad9e9dc8a0
Thomas Heissenberger <trh8614@rit.edu>
2015-02-21 17:23:39 -0500

    No Swing, No.

--- HTMLEditor/src/htmleditor/HTMLEditor.java
+++ HTMLEditor/src/htmleditor/HTMLEditor.java
@@ -10,76 +10,25 @@
  */
 package htmleditor;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import javafx.application.Platform;
-import javafx.embed.swing.JFXPanel;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.layout.StackPane;
-import javax.swing.JApplet;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
+import javafx.stage.Stage;
 
 /**
  *
  * @author trh8614
  */
-public class HTMLEditor extends JApplet {
-    
-    private static final int JFXPANEL_WIDTH_INT = 300;
-    private static final int JFXPANEL_HEIGHT_INT = 250;
-    private static JFXPanel fxContainer;
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            
-            @Override
-            public void run() {
-                try {
-                    UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-                } catch (Exception e) {
-                }
-                
-                JFrame frame = new JFrame("JavaFX 2 in Swing");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                
-                JApplet applet = new HTMLEditor();
-                applet.init();
-                
-                frame.setContentPane(applet.getContentPane());
-                
-                frame.pack();
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
-                
-                applet.start();
-            }
-        });
-    }
+public class HTMLEditor extends Application {
     
     @Override
-    public void init() {
-        fxContainer = new JFXPanel();
-        fxContainer.setPreferredSize(new Dimension(JFXPANEL_WIDTH_INT, JFXPANEL_HEIGHT_INT));
-        add(fxContainer, BorderLayout.CENTER);
-        // create JavaFX scene
-        Platform.runLater(new Runnable() {
-            
-            @Override
-            public void run() {
-                createScene();
-            }
-        });
-    }
-    
-    private void createScene() {
+    public void start(Stage primaryStage) {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -89,9 +38,30 @@ public class HTMLEditor extends JApplet {
                 System.out.println("Hello World!");
             }
         });
+        
         StackPane root = new StackPane();
+        
+        final Menu menu1 = new Menu("File");
+        final Menu menu2 = new Menu("Options");
+        final Menu menu3 = new Menu("Help");
+ 
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(menu1, menu2, menu3);
+        root.getChildren().add(menuBar);
         root.getChildren().add(btn);
-        fxContainer.setScene(new Scene(root));
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
     }
     
 }
