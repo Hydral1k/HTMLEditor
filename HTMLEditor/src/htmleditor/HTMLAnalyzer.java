f4005cb30a5499606010925bf226721c30cfd82e
Austin Laptop <Austin Laptop@129.21.125.141>
2015-02-21 16:45:50 -0500

    Added function stubs to HTMLAnalyzer.

--- HTMLEditor/src/htmleditor/HTMLAnalyzer.java
+++ HTMLEditor/src/htmleditor/HTMLAnalyzer.java
@@ -18,7 +18,7 @@ public class HTMLAnalyzer {
      * @return 
      */
     public boolean wellFormed( String bufferHTML ){
-        
+        //Must check that every open tag has a corresponding close tag.
         if(true){ // some sort of stuff to check if the HTML is good, maybe a regex function
             
             return true;
@@ -40,6 +40,28 @@ public class HTMLAnalyzer {
         // we return bufferHTML after it has been properly indented. 
         
         return bufferHTML;
+    }
+    
+    /**
+     * Counts the number of words in a given HTML buffer.
+     * @param bufferHTML
+     * @return count
+     */
+    public int wordCount( String bufferHTML ){
+        int count = 0 ;
+        //Count the number of words
+        return count ;
+    }
+    
+    /**
+     * Counts the number of lines in a given HTML buffer.
+     * @param bufferHTML
+     * @return 
+     */
+    public int lineCount( String bufferHTML ){
+        int count = 0 ;
+        //Count the number of lines
+        return count ;
     }
     
     // more functions will go in here. check the Domain Model if you would
