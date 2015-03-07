f3711589f1362c0a0ca9f1e025cfcc0faf3caeae
jlt8213 <jlt8213@rit.edu>
2015-03-06 20:48:19 -0500

    Checks for well-formed HTML on opening

--- HTMLEditor/src/htmleditor/OpenFileCommand.java
+++ HTMLEditor/src/htmleditor/OpenFileCommand.java
@@ -10,12 +10,23 @@ import javafx.event.Event;
  */
 public class OpenFileCommand implements Command {
     HTMLEditor editor ;
+    HTMLAnalyzer analyzer;
     
     public OpenFileCommand(HTMLEditor editor){
         this.editor = editor;
+        this.analyzer = new HTMLAnalyzer();
     }
     
     public void execute(Event t){
         this.editor.openFile(editor.requestFile());
+        int result;
+            if( !analyzer.wellFormed(editor.getBuffer()) ){
+                result = YesNoDialogBox.show(editor.getStage(), "The HTML file you are trying to open is not well formed.\nOpen file with reduced functionality?");
+            } else {
+                result = 1;
+            }
+            if (result == 0){
+                editor.closeCurrentTab();
+            }
     }
 }
