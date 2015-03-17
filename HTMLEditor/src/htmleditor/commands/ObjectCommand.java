093e181bd3bc8683e9143041b04b7b9a86c28a58
jlt8213 <jlt8213@rit.edu>
2015-03-17 13:35:53 -0400

    Hold my objectified buffer, I'm going in!

--- HTMLEditor/src/htmleditor/commands/ObjectCommand.java
+++ HTMLEditor/src/htmleditor/commands/ObjectCommand.java
@@ -5,6 +5,7 @@ package htmleditor.commands;
 
 import htmleditor.HTMLEditor;
 import javafx.event.Event;
+import javafx.scene.control.TextArea;
 
 
 
@@ -15,12 +16,14 @@ import javafx.event.Event;
 public class ObjectCommand implements Command {
     HTMLEditor editor ;
     
+    
     public ObjectCommand(HTMLEditor editor){
         this.editor = editor;
     }
     
     public void execute(Event t){
         System.out.println("Objectify stub");
+        TextArea thisTA = editor.getText();
         
     }
 }
