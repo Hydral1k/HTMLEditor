7105904adc55c84ca43cdf0e597a9ff193f92ae2
jlt8213 <jlt8213@rit.edu>
2015-03-17 12:55:00 -0400

    Changed ObjectCommand descriptions & imports

--- HTMLEditor/src/htmleditor/commands/ObjectCommand.java
+++ HTMLEditor/src/htmleditor/commands/ObjectCommand.java
@@ -1,22 +1,16 @@
 /*
- * The Command to create a new file.
+ * The Command to make objects out of a buffer
  */
 package htmleditor.commands;
 
-import htmleditor.CloseListener;
 import htmleditor.HTMLEditor;
-import htmleditor.MyEventHandler;
-import htmleditor.TabData;
 import javafx.event.Event;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
+
+
 
 /**
  *
- * @author aac6012
+ * @author jlt8213
  */
 public class ObjectCommand implements Command {
     HTMLEditor editor ;
@@ -27,5 +21,6 @@ public class ObjectCommand implements Command {
     
     public void execute(Event t){
         System.out.println("Objectify stub");
+        
     }
 }
