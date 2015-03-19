4f515f8c5e12ff6b5ae72c4b024877ecc71090f3
Austin Laptop <Austin Laptop@129.21.126.217>
2015-03-19 17:41:35 -0400

    Paste will now override selected text.

--- HTMLEditor/src/htmleditor/commands/PasteCommand.java
+++ HTMLEditor/src/htmleditor/commands/PasteCommand.java
@@ -17,15 +17,55 @@ public class PasteCommand extends UndoableCommand {
 
     @Override
     public void execute(Event t) {
-        int pos = this.editor.getCarrotPosition() ;
         String oldBuff = this.editor.getText().getText() ;
+        String newBuff ;
         
-        String newBuff = oldBuff.substring(0,pos) ;
-        newBuff += this.editor.getClipboard() ;
-        newBuff += oldBuff.substring(pos) ;
+        String selection = this.editor.getBufferSelection() ;
+        int pos = this.editor.getCarrotPosition() ;
+        
+        //Overwrite the selection with the pasted text.
+        if (!selection.equals("")) {
+            
+            //Text is to the left of cursor
+            if(pos + selection.length() <= oldBuff.length()
+            &&
+            selection.equals(oldBuff.substring(pos, pos+selection.length()))){
+                //Remove text from right of cursor and insert pasted text
+                newBuff = oldBuff.substring(0,pos) ;
+                newBuff += this.editor.getClipboard() ;
+                newBuff += oldBuff.substring(pos+selection.length()) ;
+            }
+            
+            //Text is to the right of cursor
+            else if(pos - selection.length() > 0 
+            &&
+            selection.equals(oldBuff.substring(pos-selection.length(), pos))){
+                //Remove text from left of cursor and insert pasted text
+                newBuff = oldBuff.substring(0,pos-selection.length()) ;
+                newBuff += this.editor.getClipboard() ;
+                newBuff += oldBuff.substring(pos) ;
+                
+            }
+            
+            //Unable to find text with respect to cursor
+            else {
+                System.out.println("You broke it.") ;
+                //Don't change the buffer if something broke (damage control)
+                newBuff = oldBuff ;
+            }
+            
+        }
+        
+        //No text to overwrite because no selection.
+        else {
+            newBuff = oldBuff.substring(0,pos) ;
+            newBuff += this.editor.getClipboard() ;
+            newBuff += oldBuff.substring(pos) ;
+        }
+        
         
         this.editor.setBuffer(newBuff) ;
         
         this.saveState() ; //Save after state changes.
-    }   
+    }
 }
