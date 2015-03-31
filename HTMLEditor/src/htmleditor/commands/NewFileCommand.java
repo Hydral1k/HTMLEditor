dc4453781b997e981d5f3dedeb0fa2bfc353b267
Jordan Tice <jlt8213@rit.edu>
2015-03-31 01:28:51 -0400

    Removed scrollbar on line numbers

--- HTMLEditor/src/htmleditor/commands/NewFileCommand.java
+++ HTMLEditor/src/htmleditor/commands/NewFileCommand.java
@@ -13,6 +13,7 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyEvent;
